01d7272b14325e5a8851ae94d68653a230b34987
Anh Truong <hunganhitc@gmail.com>
2021-10-18 10:20:49 +0700

    commit code notification update new version

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,9 +1,27 @@
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/sw_cached.js').then(function(registration) {
-        console.log('ServiceWorker registration successful');
-      }, function(err) {
-        console.log('ServiceWorker registration failed: ', err);
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('/sw_cached.js').then(function (registration) {
+      registration.addEventListener('updatefound', (newWorker) => {
+        if (navigator.serviceWorker.controller) {
+          showUpdateNewVersionPopup();
+        }
       });
+      console.log('ServiceWorker registration successful');
+    }, function (err) {
+      console.log('ServiceWorker registration failed: ', err);
     });
+  });
+}
+
+function showUpdateNewVersionPopup() {
+  var newVersionMessageToast = $("#upadateNewVersionMessageToast");
+  if (newVersionMessageToast.css("display") !== "block") {
+    newVersionMessageToast.toast({
+          autohide: false
+    }).show(); 
   }
+}
+
+function updateNewVersionSW() {
+  window.location.reload();
+}
